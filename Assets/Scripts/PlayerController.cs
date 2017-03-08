aadf96a8c76fbc170266970d2eb5902cd0169bf0
HanhVietPhan <hanhphanviet@yahoo.com>
2017-03-08 20:35:15 +0700

    sync

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,30 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class MinMax
+public class Boundary
 {
     public float xMin, xMax, zMin, zMax;
 }
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-    public MinMax minmax;
+    public Boundary boundary;
     public float tilt;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
     private Rigidbody rb;
 
 	void Start () {
 
         rb = GetComponent<Rigidbody>();
 	}
-	
-	void FixedUpdate () {
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
+    void FixedUpdate () {
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.velocity = movement * speed;
-        rb.position = new Vector3(Mathf.Clamp(rb.position.x, minmax.xMin, minmax.xMax), 0.0f, Mathf.Clamp(rb.position.z, minmax.zMin, minmax.zMax));
+        rb.position = new Vector3(Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 0.0f, Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
 
         rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
     }
